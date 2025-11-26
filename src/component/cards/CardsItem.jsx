15ddc9647d28ec4a6217d94d29b48dd4058b4fa0
mehdi-tayefe <mehditayefe1996@gmail.com>
2025-11-26 20:27:09 +0330

    fix cardsItem image border

--- src/component/cards/CardsItem.jsx
+++ src/component/cards/CardsItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function CardsItem() {
   return (
     <div className='w-full h-20 flex justify-start items-start border-b bg-purple-300'>
-      <div className='w-1/4 h-[90%] self-center pl-2 border'>
+      <div className='w-1/4 h-[90%] self-center pl-2 '>
         <div className='w-full h-full bg-green-400 border'>
 
         </div>

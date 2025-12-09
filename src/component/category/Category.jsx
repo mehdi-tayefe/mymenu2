ab01bcdbaa91db79ed45f64322066314092567f9
mehdi-tayefe <mehditayefe1996@gmail.com>
2025-12-09 20:37:17 +0330

    improve category component

--- src/component/category/Category.jsx
+++ src/component/category/Category.jsx
@@ -1,25 +1,31 @@
 import React from 'react'
 import CategoryItem from './CategoryItem'
-import soda  from "../../assets/category/soda.png"
-import cocktail  from "../../assets/category/cocktail.png"
-import soft  from "../../assets/category/soft.png"
-import cheers  from "../../assets/category/cheers.png"
-import coconut  from "../../assets/category/coconut.png"
-import juice  from "../../assets/category/juice.png"
-import mocktail  from "../../assets/category/mocktail.png"
+import Shake from "../../assets/category/soda.png"
+import cocktail from "../../assets/category/cocktail.png"
+import Shot from "../../assets/category/soft.png"
+import Beer from "../../assets/category/cheers.png"
+import Cocoa from "../../assets/category/coconut.png"
+import Punch from "../../assets/category/juice.png"
+import Other from "../../assets/category/mocktail.png"
 
 function Category() {
 
-  const catDrinks =[
-    soda , cocktail ,soft ,cheers, coconut ,juice , mocktail
-  ]
+  const catDrinks = [
+    { name: "Shake", img: Shake },
+    { name: "Cocktail", img: cocktail },
+    { name: "Shot", img: Shot },
+    { name: "Beer", img: Beer },
+    { name: "Cocoa", img: Cocoa },
+    { name: "Punch", img: Punch },
+    { name: "Other", img: Other }
+  ];
 
   return (
     <div className='flex items-center justify-start w-full h-40 gap-2 overflow-x-auto scrollbar-hidden whitespace-nowrap bg-gray-500 pl-2 shadow-lg  static top-0'>
-          {catDrinks.map((img, index) => (
-        <CategoryItem key={index} catImg={img} />
+      {catDrinks.map((item, index) => (
+        <CategoryItem key={index} catImg={item.img} name={item.name} />
       ))}
- 
+
     </div>
   )
 }
--- src/component/category/CategoryItem.jsx
+++ src/component/category/CategoryItem.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function CategoryItem({catImg}) {
+function CategoryItem({catImg , name}) {
   return (
-    <div className='border rounded-lg min-w-[90px] h-[70%] flex justify-center items-center'>
+    <div className='border rounded-lg min-w-[90px] h-[70%] flex justify-center items-center' name = {name} >
       <img className='w-[70%] h-[70%]' src={catImg} alt='drink' />
     </div>
   )

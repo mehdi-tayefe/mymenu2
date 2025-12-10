554ca5625d330085ce5835830d191fc6b0be0774
mehdi-tayefe <mehditayefe1996@gmail.com>
2025-12-10 20:32:18 +0330

    fix scroll problem

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,28 +1,28 @@
 function Footer() {
   return (
-    <div className=" bottom-0 grid w-full gap-1 grid-rows-[1fr_1fr_1fr_1fr] h-3/10">
+    <div className=" bottom-0 grid w-full gap-1 grid-rows-[1fr_1fr_1fr_1fr] h-3/10 bg-gray-600">
       <div className="flex items-center justify-center w-full h-full">
-        <h2 className="text-xl">CAFE X</h2>
+        <h2 className="text-xl text-white font-bold">CAFE X</h2>
       </div>
       <div className="flex items-center justify-center w-full h-full">
         <div className="flex flex-col items-center justify-start w-full h-full">
-          <h3 className="w-1/2 text-lg text-center border-b border-yellow-300">
+          <h3 className="w-1/2 text-lg text-center border-b border-yellow-300 text-white font-bold">
             address
           </h3>
-          <h3 className="mt-1 text-center">meydoon vanak</h3>
+          <h3 className="mt-1 text-center text-white">meydoon vanak</h3>
         </div>
       </div>
        <div className="flex items-center justify-center w-full h-full">
         <div className="flex flex-col items-center justify-start w-full h-full">
-          <h3 className="w-1/2 text-lg text-center border-b border-yellow-300">
+          <h3 className="w-1/2 text-lg text-center border-b border-yellow-300 text-white font-bold">
             contact us
           </h3>
-          <h3 className="mt-1 text-center">mobile : 09127812982</h3>
+          <h3 className="mt-1 text-center text-white">mobile : 09127812982</h3>
         </div>
       </div>
       <div className="flex items-center justify-center w-full h-full">
         <div className="flex flex-col items-center justify-start w-full h-full">
-          <h3 className="w-1/2 text-lg text-center border-b border-yellow-300">
+          <h3 className="w-1/2 text-lg text-center border-b border-yellow-300 text-white font-bold">
             social media
           </h3>
           <div className="flex items-center justify-center w-1/2 gap-4 mt-1 h-1/2">
--- src/component/layout/Layout.jsx
+++ src/component/layout/Layout.jsx
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 
 function Layout({children}) {
   return (
-    <div className='relative flex flex-col items-center justify-start w-screen min-h-screen '>
+    <div className='relative flex flex-col items-center justify-start w-full min-h-screen '>
         <Header />
         <main className="flex flex-col items-center flex-1 w-full">
            {children}

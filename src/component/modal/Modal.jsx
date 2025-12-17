384331b8cca69b3866ac8cd152089068b7fc5860
mehdi-tayefe <mehditayefe1996@gmail.com>
2025-12-17 20:23:54 +0330

    create modal component

---
+++ src/component/modal/Modal.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Modal({ isOpen, onClose, children }) {
+  if (!isOpen) return null;
+
+  return (
+    <div
+      onClick={onClose}
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black/70"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="w-[80vw] h-[80vh] bg-zinc-900 rounded-2xl p-6 relative"
+      >
+        <button
+          onClick={onClose}
+          className="absolute top-4 right-4 text-red text-xl"
+        >
+          ✕
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Modal;

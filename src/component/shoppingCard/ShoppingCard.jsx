7d4227878b41dfc523a7cd9b177809e4c549febb
mehdi-tayefe <mehditayefe1996@gmail.com>
2025-12-24 20:55:28 +0330

    dynamic shopping cart

--- src/component/shoppingCard/ShoppingCard.jsx
+++ src/component/shoppingCard/ShoppingCard.jsx
@@ -1,22 +1,44 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { getProduct } from '../../services/api';
 
-function ShoppingCard({img ,name ,count}) {
-  return (
-    <div className='w-full h-32 grid grid-cols-[2fr_2fr_1fr] gap-2 border border-black rounded-sm'>
-        <div className='w-full h-full p-2 flex justify-center items-center'>
-            <img src={img} alt='drinks' className='w-full h-full' />
+function ShoppingCard({ id, count }) {
+
+    const [product, setProduct] = useState([])
+    const fetched = useRef(false);
+
+
+    useEffect(() => {
+        if (fetched.current) return;
+        fetched.current = true;
+
+        const fetchProduct = async () => {
+            try {
+                const result = getProduct(id)
+                setProduct(result.drinks)
+            }
+            catch (error) {
+                console.error("Error fetching products:", error);
+            }
+        }
+        fetchProduct()
+    })
+
+    return (
+        <div className='w-full h-32 grid grid-cols-[2fr_2fr_1fr] gap-2 border border-black rounded-sm'>
+            <div className='w-full h-full p-2 flex justify-center items-center'>
+                <img src={product.drinks[0].strDrinkThumb} alt='drinks' className='w-full h-full' />
+            </div>
+            <div className='w-full h-full p-4 flex flex-col justify-between items-center'>
+                <div className='text-xl font-bold'>{product.drinks[0].strDrink}</div>
+                <div className='text-xl font-bold self-start'>99$</div>
+            </div>
+            <div className='w-full h-full flex flex-col justify-center items-center gap-10'>
+                <div className='w-24 h-24 text-center bg-gray-500 cursor-pointer'>+</div>
+                <div className='w-6 h-12 text-center'>{count}</div>
+                <div className='w-24 h-24 text-center bg-gray-500 cursor-pointer'>-</div>
+            </div>
         </div>
-        <div className='w-full h-full p-4 flex flex-col justify-between items-center'>
-            <div className='text-xl font-bold'>{name}</div>
-            <div className='text-xl font-bold self-start'>99$</div>
-        </div>
-        <div className='w-full h-full flex flex-col justify-center items-center gap-10'>
-            <div className='w-24 h-24 text-center bg-gray-500 cursor-pointer'>+</div>
-            <div className='w-6 h-12 text-center'>{count}</div>
-            <div className='w-24 h-24 text-center bg-gray-500 cursor-pointer'>-</div>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default ShoppingCard
